bb35489883815d6234d8eb0c0b76208d0c53feeb
shreyakansagra <155213461+shreyakansagra@users.noreply.github.com>
2024-01-10 11:15:52 +0530

    Add files via upload

---
+++ dynemic _table.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+
+<form method="get" action="table.php">
+
+
+Enter number for start: <input type="text" name="sno"><br>
+
+Enter number for end: <input type="text" name="lno"><br>
+
+<input type="submit" name="submit">
+
+</form>
+</body>
+</head>
+</html>
